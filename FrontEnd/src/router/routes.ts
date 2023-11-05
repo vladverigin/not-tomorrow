e22cb3c45c4386360a4ef15e5df28deea3045ea4
Vlad Verigin <vladver233@gmail.com>
2023-11-06 02:40:15 +0300

    Update routes.ts

--- FrontEnd/src/router/routes.ts
+++ FrontEnd/src/router/routes.ts
@@ -1,10 +1,44 @@
 import { RouteRecordRaw } from 'vue-router';
+import authGuard from "src/router/auth-guard";
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    meta: {
+      requiresAuth:true
+    },
+    beforeEnter:authGuard
+  },
+  {
+    path: '/reports',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/ReportsPage.vue') }],
+    meta: {
+      requiresAuth:true
+    },
+    beforeEnter:authGuard
+  },
+  {
+    path: '/auth',
+    component: () => import('layouts/EmptyLayout.vue'),
+    children: [
+      {
+        name:'login',
+        path: 'login',
+        component: () => import('pages/LoginPage.vue'),
+
+      }
+    ],
+    meta: {
+      requiresAuth:false
+    },
+    beforeEnter:authGuard
+  },
+  {
+    path: '/welcome',
+    component: () => import('layouts/LandingLayout.vue'),
   },
 
   // Always leave this as last one,
